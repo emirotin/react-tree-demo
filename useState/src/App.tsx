b18a77a111e184e825e336223cf245b24cb958f0
Eugene Mirotin <emirotin@gmail.com>
2024-12-02 15:16:08 +0200

    rework for plain tree representation

--- useState/src/App.tsx
+++ useState/src/App.tsx
@@ -1,72 +1,82 @@
 import { faker } from "@faker-js/faker";
-import { Fragment, useMemo, useState } from "react";
+import { nanoid } from "nanoid";
+import { useMemo, useState } from "react";
+import { produce } from "immer";
 import {
   Folder as FolderIcon,
   File as FileIcon,
   FilePlus as FilePlusIcon,
+  FolderPlus as FolderPlusIcon,
 } from "react-feather";
 
-interface File {
+interface BaseNode {
   name: string;
+  id: string;
+  parentId: string | null;
+}
+
+interface File extends BaseNode {
   size: number;
-  parent: Folder | FileSystem;
+  parentId: string;
   __type: "file";
 }
 
-interface Folder {
-  name: string;
-  children: Array<File | Folder>;
-  parent: Folder | FileSystem;
+interface Folder extends BaseNode {
+  childrenIds: string[];
+  parentId: string;
   __type: "folder";
 }
 
-interface FileSystem extends Omit<Folder, "__type" | "parent"> {
-  name: "<root>";
-  parent: null;
+const ROOT_ID = "<root>";
+
+interface Root extends BaseNode {
+  name: typeof ROOT_ID;
+  childrenIds: string[];
+  parentId: null;
   __type: "root";
 }
 
-const makeFile = (parent: Folder | FileSystem) =>
+type Node = File | Folder | Root;
+
+interface FileSystem {
+  nodes: Record<string, Node>;
+}
+
+const makeFile = (parentId: string): File =>
   ({
+    id: nanoid(),
+    parentId,
     name: faker.system.commonFileName(),
     size: Math.floor(Math.random() * 10_000_000 + 3_000),
-    parent,
     __type: "file",
   } satisfies File);
 
-const makeFolder = (parent: Folder | FileSystem) =>
+const makeFolder = (parentId: string): Folder =>
   ({
+    id: nanoid(),
+    parentId,
     name: faker.system.commonFileName().split(".")[0],
-    children: [] as Array<File | Folder>,
-    parent,
+    childrenIds: [] as string[],
     __type: "folder",
   } satisfies Folder);
 
-const makeFS = () =>
+const makeRoot = (): Root =>
   ({
-    name: "<root>",
-    children: [] as Array<File | Folder>,
-    parent: null,
+    id: ROOT_ID,
+    parentId: null,
+    name: ROOT_ID,
+    childrenIds: [] as string[],
     __type: "root",
-  } satisfies FileSystem);
+  } satisfies Root);
 
-const generateFS = () => {
-  const fs = makeFS();
-  for (let i = 0; i < 6; i++) {
-    const f = makeFolder(fs);
-    fs.children.push(f);
-    for (let j = 0; j < 5; j++) {
-      const f1 = makeFolder(f);
-      f.children.push(f1);
-      for (let j = 0; j < 5; j++) {
-        f1.children.push(makeFile(f1));
-      }
-    }
-    for (let j = 0; j < 5; j++) {
-      f.children.push(makeFile(f));
-    }
-  }
-  return fs;
+const makeFS = (): FileSystem => {
+  const root = makeRoot();
+
+  return {
+    nodes: {
+      [root.id]: root,
+    },
+  } satisfies FileSystem;
 };
 
 const formatSize = (size: number) => {
@@ -76,12 +86,53 @@ const formatSize = (size: number) => {
   return `${(size / 1_000).toFixed(2)} KB`;
 };
 
-const getSize = (item: File | Folder | FileSystem): number => {
+const getSize = (fs: FileSystem, itemId: string): number => {
+  const item = fs.nodes[itemId];
+  if (!item) {
+    return 0;
+  }
+
   if (item.__type === "file") {
     return item.size;
   }
-  return 4_000 + item.children.map(getSize).reduce((acc, x) => acc + x, 0);
+  return (
+    4_000 +
+    item.childrenIds.map((id) => getSize(fs, id)).reduce((acc, x) => acc + x, 0)
+  );
 };
+
+function NodePresentation({
+  fs,
+  id,
+  onAddFile,
+  onAddFolder,
+}: {
+  fs: FileSystem;
+  id: string;
+  onAddFile: (parentId: string) => void;
+  onAddFolder: (parentId: string) => void;
+}) {
+  const item = fs.nodes[id];
+  const size = useMemo(() => (item ? getSize(fs, id) : 0), [fs, item, id]);
+
+  if (!item) {
+    return null;
+  }
+
+  if (item.__type === "file") {
+    return <FilePresentation file={item} />;
+  }
+
+  return (
+    <FolderPresentation
+      fs={fs}
+      folder={item}
+      size={size}
+      onAddFile={onAddFile}
+      onAddFolder={onAddFolder}
+    />
+  );
+}
 
 function FilePresentation({ file }: { file: File }) {
   return (
@@ -94,77 +145,83 @@ function FilePresentation({ file }: { file: File }) {
 }
 
 function FolderPresentation({
+  fs,
   folder,
+  size,
   onAddFile,
+  onAddFolder,
 }: {
-  folder: Folder | FileSystem;
-  onAddFile: (folder: Folder | FileSystem) => void;
+  fs: FileSystem;
+  folder: Folder | Root;
+  size: number;
+  onAddFile: (parentId: string) => void;
+  onAddFolder: (parentId: string) => void;
 }) {
-  const size = useMemo(() => getSize(folder), [folder]);
-
   return (
     <div>
       <div className="flex flex-row gap-2 items-center group">
         <FolderIcon size={16} />
         <strong>{folder.name}</strong>
         <em>{formatSize(size)}</em>
-        <span className="hidden group-hover:inline-block">
-          <button type="button" onClick={() => onAddFile(folder)}>
+        <span className="hidden group-hover:flex gap-1">
+          <button type="button" onClick={() => onAddFolder(folder.id)}>
+            <FolderPlusIcon size={16} />
+          </button>
+          <button type="button" onClick={() => onAddFile(folder.id)}>
             <FilePlusIcon size={16} />
           </button>
         </span>
       </div>
       <div className="pl-4 border-l">
-        {folder.children.map((item, i) => (
-          // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-          <Fragment key={i}>
-            {item.__type === "file" ? (
-              <FilePresentation file={item} />
-            ) : (
-              <FolderPresentation folder={item} onAddFile={onAddFile} />
-            )}
-          </Fragment>
+        {folder.childrenIds.map((id) => (
+          <NodePresentation
+            key={id}
+            id={id}
+            onAddFile={onAddFile}
+            onAddFolder={onAddFolder}
+            fs={fs}
+          />
         ))}
       </div>
     </div>
   );
 }
 
-const replaceChild = <T extends Folder | FileSystem>(
-  parent: T,
-  item: Folder
-) => {
-  return {
-    ...parent,
-    children: parent.children.map((c) => (c.name === item.name ? item : c)),
-  } as T;
-};
-
-const addFile = (parent: Folder | FileSystem): FileSystem => {
-  const newParent = {
-    ...parent,
-    children: [...parent.children, makeFile(parent)],
-  } as Folder | FileSystem;
-  if (newParent.__type === "root") {
-    return newParent;
+const addItem = (
+  fs: FileSystem,
+  parentId: string,
+  type: "file" | "folder"
+): FileSystem => {
+  const parent = fs.nodes[parentId];
+  if (!parent || parent.__type === "file") {
+    throw new Error("Cannot add file as a child of another file");
   }
-  let result: Folder | FileSystem = newParent;
-  while (result.parent !== null) {
-    result = replaceChild(result.parent, result);
-  }
-  return result;
+  return produce(fs, (draft) => {
+    const newItem = type === "file" ? makeFile(parentId) : makeFolder(parentId);
+    draft.nodes[newItem.id] = newItem;
+    (draft.nodes[parentId] as Folder | Root).childrenIds.push(newItem.id);
+  });
 };
 
 function App() {
-  const [fs, setFS] = useState(generateFS);
+  const [fs, setFS] = useState(makeFS);
 
-  const onAddFile = (folder: Folder | FileSystem) => {
-    setFS(addFile(folder));
+  const onAddFile = (parentId: string) => {
+    setFS(addItem(fs, parentId, "file"));
+  };
+
+  const onAddFolder = (parentId: string) => {
+    setFS(addItem(fs, parentId, "folder"));
   };
 
   return (
     <div className="p-4">
-      <FolderPresentation folder={fs} onAddFile={onAddFile} />
+      <NodePresentation
+        fs={fs}
+        id={ROOT_ID}
+        onAddFile={onAddFile}
+        onAddFolder={onAddFolder}
+      />
     </div>
   );
 }
